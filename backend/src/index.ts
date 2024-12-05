aca5314e3551cd2d17862678f61e27f672951794
David Ho <kietho.060193>
2024-12-05 21:34:09 +0700

    16 Cloudinary Config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,14 @@ import 'dotenv/config';
 import userRoute from './routes/user';
 import authRoute from './routes/auth';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+
+// Config cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING as string)

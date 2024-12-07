c33b3d733fea2fb46fb6d4138730b5eb82d04794
David Ho <kietho.060193>
2024-12-07 21:38:17 +0700

    Search Page Fetch Hotels

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,4 @@
-import { HotelType } from './../../backend/src/shared/types';
+import { HotelSearchRespone, HotelType } from './../../backend/src/shared/types';
 import { RegisterFormData } from './pages/Register';
 import { SignInFormData } from './pages/SignIn';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
@@ -111,6 +111,35 @@ export const updateMyHotelById = async (hotelFormData: FormData) => {
   );
 
   if (!res.ok) throw new Error('Failed to update Hotel');
+  return await res.json();
+};
+
+/* TYPE SearchParams */
+export type SearchParams = {
+  destination?: string;
+  checkIn?: string;
+  checkOut?: string;
+  adultCount?: string;
+  childCount?: string;
+  page?: string;
+};
+
+/* searchHotels */
+export const searchHotels = async (
+  searchParams: SearchParams,
+): Promise<HotelSearchRespone> => {
+  // Prepare query
+  const queryParams = new URLSearchParams();
+  queryParams.append('destination', searchParams.destination || '');
+  queryParams.append('checkIn', searchParams.checkIn || '');
+  queryParams.append('checkOut', searchParams.checkOut || '');
+  queryParams.append('adultCount', searchParams.adultCount || '');
+  queryParams.append('childCount', searchParams.childCount || '');
+  queryParams.append('page', searchParams.page || '');
+
+  // Call API
+  const res = await fetch(`${API_BASE_URL}/api/hotels/search?${queryParams}`);
+  if (!res.ok) throw new Error('Error fetching hotels');
   return await res.json();
 };
 

74eecd4fd4818fd5ab9a7745a4ce96d7e6c5b313
David Ho <kietho.060193>
2024-12-07 15:09:22 +0700

    23 Fix Show Toast

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,9 +2,11 @@ import { useMutation, useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import * as apiClient from '../api-client';
 import ManageHotelForm from '../forms/ManageHotelForm/ManageHotelForm';
+import { useAppContext } from '../contexts/AppContext/useAppContext';
 
 const EditHotel = () => {
   const { hotelId } = useParams();
+  const { showToast } = useAppContext();
 
   const { data: hotel } = useQuery(
     'fetchMyHotelById',
@@ -15,8 +17,12 @@ const EditHotel = () => {
   );
 
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: () => {},
-    onError: () => {},
+    onSuccess: () => {
+      showToast({ type: 'SUCCESS', message: 'Hotel Saved!' });
+    },
+    onError: () => {
+      showToast({ type: 'ERROR', message: 'Error Saving Hotel' });
+    },
   });
 
   const handleSave = (hotelFormData: FormData) => {
